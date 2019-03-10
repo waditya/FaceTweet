a95824d7bea1367fc1f551d8b672792f6600b932
waditya <waditya02@gmail.com>
2019-03-10 01:12:42 -0600

    Basic Server Side Rendering

--- server/express.js
+++ server/express.js
@@ -1,52 +1,98 @@
-import devBundle from './devBundle' /* Only for development mode */
 import express from 'express'
+import path from 'path'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
-import path from 'path'
-
 import Template from './../template'
-import authRoutes from './routes/auth.routes'
 import userRoutes from './routes/user.routes'
+import authRoutes from './routes/auth.routes'
 
-//To serve static files to express
+// modules for server side rendering
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import MainRouter from './../client/MainRouter'
+import StaticRouter from 'react-router-dom/StaticRouter'
 
+import { SheetsRegistry } from 'react-jss/lib/jss'
+import JssProvider from 'react-jss/lib/JssProvider'
+import { MuiThemeProvider, createMuiTheme, createGenerateClassName } from 'material-ui/styles'
+import { indigo, pink } from 'material-ui/colors'
+//end
 
-const app= express()
+//comment out before building for production
+import devBundle from './devBundle'
+
 const CURRENT_WORKING_DIR = process.cwd()
+const app = express()
+
+//comment out before building for production
 devBundle.compile(app)
 
+// parse body params and attache them to req.body
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+// secure apps by setting various HTTP headers
+app.use(helmet())
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors())
 
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 
-/* Configure Express */
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(cookieParser())
-app.use(compress())
-app.use(helmet())
-app.use(cors())
-
-app.use('/', authRoutes)
+// mount routes
 app.use('/', userRoutes)
+app.use('/', authRoutes)
 
+app.get('*', (req, res) => {
+   const sheetsRegistry = new SheetsRegistry()
+   const theme = createMuiTheme({
+     palette: {
+       primary: {
+       light: '#757de8',
+       main: '#3f51b5',
+       dark: '#002984',
+       contrastText: '#fff',
+     },
+     secondary: {
+       light: '#ff79b0',
+       main: '#ff4081',
+       dark: '#c60055',
+       contrastText: '#000',
+     },
+       openTitle: indigo['400'],
+       protectedTitle: pink['400'],
+       type: 'light'
+     },
+   })
+   const generateClassName = createGenerateClassName()
+   const context = {}
+   const markup = ReactDOMServer.renderToString(
+      <StaticRouter location={req.url} context={context}>
+         <JssProvider registry={sheetsRegistry} generateClassName={generateClassName}>
+            <MuiThemeProvider theme={theme} sheetsManager={new Map()}>
+              <MainRouter/>
+            </MuiThemeProvider>
+         </JssProvider>
+      </StaticRouter>
+     )
+    if (context.url) {
+      return res.redirect(303, context.url)
+    }
+    const css = sheetsRegistry.toString()
+    res.status(200).send(Template({
+      markup: markup,
+      css: css
+    }))
+})
 
-//Handle authentication related erros thrown by express-jwt while JWT token validation during incoming requests
+// Catch unauthorised errors
 app.use((err, req, res, next) => {
   if (err.name === 'UnauthorizedError') {
-    res.status(401).json({
-      "error" : err.name + ": " + err.message
-    })
+    res.status(401).json({"error" : err.name + ": " + err.message})
   }
 })
-
-
-app.get('/', (req, res)=>{
-  res.status(200).send(Template())
-})
-
 
 export default app
