0c0cba56175917d181083bd87daeee02ae124d3f
waditya <waditya02@gmail.com>
2019-03-09 23:12:38 -0600

    Added Helper Methods to authenticate state across the components

---
+++ client/auth/auth-helper.js
@@ -0,0 +1,38 @@
+// Helper methods to manage auth state across the components
+// Custom PrivateRoute component to add protected routes to the frontend.
+
+// Helper methods to store and retrieve JWT credentials from client-side sessionStorage, and also clear out the
+// sessionStorage on user sign-out
+
+
+// Save credentials on successful sign-in
+
+authenticate(jwt, cb) {
+  if(typeof window !== "undefined")
+    sessionStorage.setItem('jwt', JSON.stringify(jwt))
+    cb()
+}
+
+// Retrieve credentials if signed-in
+
+isAuthenticated() {
+  if (typeof window == "undefined")
+    return false
+    if (sessionStorage.getItem('jwt'))
+      return JSON.parse(sessionStorage.getItem('jwt'))
+    else
+      return false
+}
+
+
+// signout
+
+signout(cb) {
+  if(typeof window !== "undefined")
+    sessionStorage.removeItem('jwt')
+  cb()
+  signout().then((data) => {
+      document.cookie = "t=; expires=Thu, 01 Jan 1970 00:00:00
+      UTC; path=/;"
+    })
+}
