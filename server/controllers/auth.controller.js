4651fb17c9f03eca1c2419dfeefdf65e3b3b8c47
waditya <waditya02@gmail.com>
2019-03-10 18:13:57 -0500

    Updated Auth Controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -6,37 +6,39 @@ import config from './../../config/config'
 const signin = (req, res) => {
   User.findOne({
     "email": req.body.email
-  }, (err, user)=>{
-    if(err || !user) //If there is an error or the user doesn't exist
+  }, (err, user) => {
+
+    if (err || !user)
       return res.status('401').json({
         error: "User not found"
       })
 
-      if(!user.authenticate(req.body.password)){
-        return res.status('401').send({
-          error: "Email and password don't match."
-        })
-      }
-
-      const token = jwt.sign({
-        _id : user._id
-      }, config.jwtSecret)
-
-      res.cookie("t", token, {
-        expire: new Date() + 9999
+    if (!user.authenticate(req.body.password)) {
+      return res.status('401').send({
+        error: "Email and password don't match."
       })
+    }
 
-      return res.json({
-        token,
-        user:{_id: user._id, name: user.name, email: user.email}
-      })
+    const token = jwt.sign({
+      _id: user._id
+    }, config.jwtSecret)
+
+    res.cookie("t", token, {
+      expire: new Date() + 9999
+    })
+
+    return res.json({
+      token,
+      user: {_id: user._id, name: user.name, email: user.email}
+    })
+
   })
 }
 
 const signout = (req, res) => {
   res.clearCookie("t")
   return res.status('200').json({
-    message: "Signed out"
+    message: "signed out"
   })
 }
 
@@ -47,11 +49,17 @@ const requireSignin = expressJwt({
 
 const hasAuthorization = (req, res, next) => {
   const authorized = req.profile && req.auth && req.profile._id == req.auth._id
-  if(!(authorized)){
+  if (!(authorized)) {
     return res.status('403').json({
       error: "User is not authorized"
     })
   }
+  next()
 }
 
-export default {signin, signout, requireSignin, hasAuthorization}
+export default {
+  signin,
+  signout,
+  requireSignin,
+  hasAuthorization
+}
