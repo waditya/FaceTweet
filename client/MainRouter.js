7f7fa4763f407e755dcd9d9636b005467a85c0ea
waditya <waditya02@gmail.com>
2019-03-10 01:18:07 -0600

    Final COmmit

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -11,12 +11,13 @@ import Menu from './core/Menu'
 
 class MainRouter extends Component {
   // Removes the server-side injected CSS when React component mounts
+  /* 
  componentDidMount() {
    const jssStyles = document.getElementById('jss-server-side')
    if (jssStyles && jssStyles.parentNode) {
      jssStyles.parentNode.removeChild(jssStyles)
    }
- }
+ } */
 
   render() {
     return (
--- client/main.js
+++ client/main.js
@@ -1,5 +1,13 @@
 import React from 'react'
-import { render } from 'react-dom'
+import { hydrate } from 'react-dom'
 import App from './App'
 
-render(<App/>, document.getElementById('root'))
+/*
+The hydrate function hydrates a container that already has HTML content rendered by
+ReactDOMServer. This means the server-rendered markup is preserved and only eventhandlers
+are attached when React takes over in the browser, allowing the initial load
+performance to be better.
+
+*/
+
+hydrate(<App/>, document.getElementById('root'))
