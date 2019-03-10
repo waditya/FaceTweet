50414a46bc95dd43e78ccb53bed989659940bbae
waditya <waditya02@gmail.com>
2019-03-10 18:02:17 -0500

    Update

--- client/core/Home.js
+++ client/core/Home.js
@@ -12,8 +12,7 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 5
   },
   title: {
-    padding:`${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px
-    ${theme.spacing.unit * 2}px`,
+    padding:`${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px ${theme.spacing.unit * 2}px`,
     color: theme.palette.text.secondary
   },
 media: {
@@ -25,22 +24,18 @@ class Home extends Component {
   render() {
     const {classes} = this.props
       return (
-        <div>
           <Card className={classes.card}>
             <Typography type="headline" component="h2" className={classes.title}>
               Home Page
-              </Typography>
-          <CardMedia className={classes.media} image={seashellImg} title="Unicorn Shells"/>
-          <CardContent>
-            <Typography type="body1" component="p">
-              Welcome to the FaceTweet home page
             </Typography>
-          </CardContent>
-  </Card>
-  <Link to="/users">Users</Link>
-  </div>
-
-  )
+            <CardMedia className={classes.media} image={seashellImg} title="Unicorn Shells"/>
+              <CardContent>
+                <Typography type="body1" component="p">
+                  Welcome to the FaceTweet home page
+                </Typography>
+              </CardContent>
+          </Card>
+    )
   }
 }
 
--- server/devBundle.js
+++ server/devBundle.js
@@ -5,15 +5,16 @@ import webpackHotMiddleware from 'webpack-hot-middleware'
 import webpackConfig from './../webpack.config.client.js'
 
 const compile = (app) => {
-if(config.env === "development"){
-const compiler = webpack(webpackConfig)
-const middleware = webpackMiddleware(compiler, {
-publicPath: webpackConfig.output.publicPath
-})
-app.use(middleware)
-app.use(webpackHotMiddleware(compiler))
+  if(config.env === "development"){
+    const compiler = webpack(webpackConfig)
+    const middleware = webpackMiddleware(compiler, {
+      publicPath: webpackConfig.output.publicPath
+    })
+    app.use(middleware)
+    app.use(webpackHotMiddleware(compiler))
+  }
 }
-}
+
 export default {
-compile
+  compile
 }
